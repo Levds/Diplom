248efda5f56d9591c537c62e6b823021269da700
Denis <56105273+Levds@users.noreply.github.com>
2022-12-29 19:37:56 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -2,18 +2,33 @@ import pandas as pd
 import datetime as dt
 import snscrape.modules.twitter as sntwitter
 import streamlit as st
+import pymongo
+
+
+# client = pymongo.MongoClient("mongodb+srv://user:a3AxwHpoiKLPuTn1@cluster.rlullov.mongodb.net/?retryWrites=true&w=majority")
+# db = client.tw
+# coll = db.tweets
+# coll.insert_many(
+#     {
+#     "_id" : 1,
+#     "D/T" : '',
+#     "ID" : '',
+#     "txt" : '',
+#     "User" : ''
+#     }
+# )
 
 max = 100
 list = []
 
-for i, tweet in enumerate(sntwitter.TwitterSearchScraper('into:').get_items()):
+for i, tweet in enumerate(sntwitter.TwitterSearchScraper('into:christmas').get_items()):
     if i > max:
         break
-    list.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
+    list.append([tweet.date,tweet.id,tweet.content,tweet.user.username])
 
 df = pd.DataFrame(list, columns=['D/T', 'ID', 'txt', 'User'])
 df.head()
-df.to_csv('tweets.csv', sep=',', index=False)
+df.to_csv('tweets.xls', sep=',', index=False)
 
 st.title("Streamlit Twitter Scrapper")
 seg = dt.date.today()
